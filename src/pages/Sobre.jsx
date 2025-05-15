79a653be87b1325b89fead1f3b5acbec4fa19f19
Thalis <thalisbdrago@gmail.com>
2025-05-15 11:58:29 -0300

    Atualizacoes

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,95 +1,137 @@
 'use client'
 import React from 'react';
-import image1 from "../assets/IMG_5566.JPG"
-import image2 from "../assets/IMG_1299.JPG"
-import image3 from "../assets/IMG_1301.JPG"
-import image5 from "../assets/IMG_1316.JPG"
-import image4 from "../assets/IMG_1327.JPG"
+import image1 from "../assets/IMG_7083.JPG"
+import image2 from "../assets/IMG_2857.JPG"
+import image3 from "../assets/IMG_2858.JPG"
+import image4 from "../assets/IMG_5217.JPG"
+import image5 from "../assets/IMG_2858.JPG"
+import image6 from "../assets/IMG_5566.JPG"
+
+import { MessageCircle } from "lucide-react";
+
+
 
 const Sobre = () => {
   return (
-    <div>
-        <div className="bg-gray-50 text-black py-8">
-            <div className="max-w-7xl mx-auto px-6">
-                {/* Seção "Sobre" com foto e texto */}
-                <div className="flex flex-col md:flex-row items-center justify-between">
-                {/* Foto de João */}
-                <div className="md:w-1/2 flex justify-center mb-8 md:mb-0">
-                    <img 
-                    src={image1} 
-                    alt="João Martins"
-                    className="w-120 h-auto rounded-lg shadow-lg" // Ajustando a largura e altura da imagem
-                    />
-                </div>
-
-                {/* Texto sobre João */}
-                <div className="md:w-1/2 text-center md:text-left">
-                    <h2 className="text-4xl font-extrabold text-gray-900 font-global">Sobre João Martins</h2> {/* Aplicando a fonte Global */}
-                    <p className="mt-4 text-lg text-gray-900 font-global">
-                    Com mais de 10 anos de experiência no cenário musical, eu sou João Martins, um baixista e produtor musical apaixonado pela arte de criar e transformar músicas. Minha trajetória me levou a colaborar com grandes nomes da música gospel, como Felipe Vilela, Claudio Claro, Sarah Oliveira, e atualmente estou em turnê com 2Metro e Salomão do Reggae. Além disso, sou criador do Método GrooveDoZero, uma mentoria exclusiva para baixistas iniciantes e avançados, onde ensino e ajudo meus alunos a se inserirem de verdade no mercado musical.
-                    </p>
-                    <p className="mt-4 text-lg text-gray-900 font-global">
-                    Com mais de 1 milhão de visualizações no YouTube e Instagram com meus vídeos, minha missão é compartilhar meu conhecimento e experiência com a próxima geração de músicos.
-                    </p>
-                </div>
-                </div>
-            </div>
+    <div className="min-h-screen text-white bg-neutral-800 font-montserrat">
+      {/* Seção 1 - Sobre */}
+      <div className="w-full bg-neutral-800 grid md:grid-cols-2 p-6 gap-10 items-center">
+        <div>
+          <h1 className="text-4xl md:text-4xl text-center font-bold mb-2 font-osvaldo">Sobre João Martins</h1>
+          <h2 className="text-sm text-center mb-6 font-osvaldo">(Baixista e Produtor Musical)</h2>
+          <p className="mb-4 text-lg md:text-1xl">
+            Com mais de 10 anos de experiência no cenário musical, João Martins atua como baixista e produtor, tendo colaborado com artistas renomados como 2 Metro e Salomão do Reggae. Sua técnica refinada e criatividade musical são essenciais para dar vida a novos projetos, sempre buscando inovar e explorar novos horizontes sonoros.
+          </p>
+          <p className="mb-4 text-lg md:text-1xl">
+            Além de seu trabalho como músico, João também é criador do Groovedozero, um método de ensino inovador que tem transformado a vida de muitos baixistas, tanto no Brasil quanto no exterior. Seu método é reconhecido por sua abordagem prática e acessível, permitindo que músicos desenvolvam uma base sólida e um groove único no baixo, independentemente do nível de experiência.
+          </p>
+          <p className="mb-4 text-lg md:text-1xl">
+            Com um estilo único e uma abordagem focada na musicalidade e técnica, João Martins continua a trilhar seu caminho na música, sempre em busca de novos desafios e possibilidades sonoras.
+          </p>
         </div>
+        <div className="w-full h-124 rounded-2x1 overflow-hidden shadow-lg">
+            <img
+                src={image2}
+                alt="João Martins sentado com baixo"
+                className="object-cover w-full h-full"
+            />
+        </div>
+      </div>
 
-    <div className='bg-gray-50 text-black py-12'>
-        <div className="max-w-7xl mx-auto px-6 flex flex-col lg:flex-row gap-20">
-            <div className="lg:w-1/1">
-                <h2 className="text-4xl font-extrabold text-gray-900 font-global">Minha Trajetória Profissional</h2>
-                <p className='mt-4 text-lg text-gray-900 font-global'>
-                    Artistas e Projetos
-                    Ao longo da minha carreira, tive a honra de trabalhar com artistas de renome, tanto em estúdios quanto ao vivo. Alguns dos meus trabalhos incluem:
-                    <ul className="list-disc pl-6">
-                        <li>Felipe Vilela</li>
-                        <li>Cláudio Claro</li>
-                        <li>Sarah Oliveira</li>
-                        <li>Salomão do Reggae (Atualmente)</li>
-                        <li>2Metro (Atualmente)</li>
-                    </ul>
-                    Cada experiência me proporcionou novos desafios, crescimento musical e oportunidades para expandir minhas habilidades como baixista e produtor.
-                </p>
-                <h2 className="text-4xl font-extrabold text-gray-900 font-global mt-10">Conquistas e Reconhecimentos</h2>
-                <p className='mt-4 text-lg text-gray-900 ' >
-                1+ Milhão de Visualizações nas minhas redes sociais (YouTube, Instagram).Endorse de Marcas Reconhecidas:
-                    <ul className="list-disc pl-6">
-                        <li>D'mark</li>
-                        <li>Aslan Guitars</li>
-                        <li>Soundvoice</li>
-                        <li>Fones KZ</li>
-                    </ul>
-                    <p className='mt-4 text-lg text-gray-900 font-global'>Essas parcerias não apenas validam meu trabalho, mas também me permitem trazer o melhor em termos de qualidade e inovação para o meu público e alunos.
-                    </p>
-                </p>
-            </div>
-            <div className="lg:w-2/2 mt-6 lg:mt-0 grid grid-cols-2 gap-4">
-                {/* Imagens da galeria */}
-                <img 
-                    src={image1} 
-                    alt="João Martins"
-                    className="w-full h-auto rounded-lg shadow-lg" 
-                />
-                <img 
-                    src={image2} 
-                    alt="João Martins"
-                    className="w-full h-auto rounded-lg shadow-lg" 
-                />
-                <img 
-                    src={image3} 
-                    alt="João Martins"
-                    className="w-full h-auto rounded-lg shadow-lg" 
-                />
-                <img 
-                    src={image5} 
-                    alt="João Martins"
-                    className="w-full h-auto rounded-lg shadow-lg" 
-                />
-            </div>
+      {/* Seção 2 - Trajetória Profissional */}
+      <div className="w-full bg-neutral-900 grid md:grid-cols-2 gap-10 p-6">
+    {/* Coluna de imagens */}
+    <div className="grid grid-cols-2 grid-rows-2 gap-4 h-full">
+        <div className="row-span-2 col-span-1 rounded-1xl overflow-hidden">
+        <img src={image6} alt="João tocando baixo" className="w-full h-full object-cover aspect-[3/4]" />
+        </div>
+        <div className="rounded-2xl overflow-hidden">
+        <img src={image3} alt="Show com 2Metro" className="w-full h-full object-cover aspect-video" />
+        </div>
+        <div className="rounded-2xl overflow-hidden">
+        <img src={image1} alt="De costas com jaqueta" className="w-full h-full object-cover aspect-video" />
         </div>
     </div>
+
+    {/* Coluna de texto */}
+        <div className="relative flex flex-col justify-start">
+            <h2 className="text-4xl md:text-4xl font-bold mb-4 font-osvaldo">Minha Trajetória Profissional</h2>
+            <p className="mb-4 text-2xl md:text-2xl">Meus trabalhos atuais incluem:</p>
+            <ul className="list-disc list-inside text-base md:text-base mb-4">
+                <li>Salomão do Reggae</li>
+                <li>2Metro</li>
+            </ul>
+            <p className="md:text-base text-base">
+                E participei de trabalhos com inúmeros outros artistas do cenário musical. Cada projeto contribuiu para meu crescimento musical, desenvolvimento de novas habilidades e ampliação da experiência como baixista e produtor.
+            </p>
+
+            {/* Asterisco posicionado no canto inferior direito */}
+            <span className="absolute bottom-0 right-0 text-9xl text-gray-400">*</span>
+        </div>
+    </div>
+
+{/* Seção 3 - Contato */}
+        <div className="relative w-full h-screen">
+        {/* Imagem de fundo ocupando toda a section */}
+        <img
+            src={image5}
+            alt="Show ao vivo com João Martins"
+            className="absolute top-0 left-0 w-full h-full object-cover"
+        />
+
+        {/* Sobreposição escura leve (opcional) */}
+        <div className="absolute top-0 left-0 w-full h-full bg-black/30" />
+
+        {/* Formulário de contato no canto esquerdo */}
+        <div className="relative z-10 flex items-center h-full pl-0 md:pl-10">
+            <div className="bg-white/90 text-black p-10 md:p-12 rounded-lg shadow-xl max-w-md w-full">
+            <h2 className="text-3xl md:text-4xl font-bold mb-6">Entre em Contato</h2>
+
+            <form className="space-y-4">
+                <input
+                type="text"
+                placeholder="Nome"
+                className="w-full p-3 border border-gray-300 rounded"
+                />
+                <input
+                type="email"
+                placeholder="E-mail"
+                className="w-full p-3 border border-gray-300 rounded"
+                />
+                <input
+                type="tel"
+                placeholder="Telefone"
+                className="w-full p-3 border border-gray-300 rounded"
+                />
+                <textarea
+                rows="4"
+                type="text"
+                placeholder="Mensagem"
+                className="w-full p-3 border border-gray-300 rounded"
+                />
+                <button
+                type="submit"
+                className="w-full bg-black text-white py-3 rounded hover:bg-gray-800 transition"
+                >
+                ENVIAR MENSAGEM
+                </button>
+            </form>
+            </div>
+        </div>
+        </div>
+
+
+
+
+      {/* Ícone no rodapé */}
+      <footer className="bg-neutral-800 text-white py-8">
+      <div className="container mx-auto">
+        <div className="text-center">
+          <p className="text-gray-400 text-sm">© 2025 Joao Martins Bass. Todos os direitos reservados.</p>
+        </div>
+      </div>
+    </footer>
+
     </div>
 
   );
